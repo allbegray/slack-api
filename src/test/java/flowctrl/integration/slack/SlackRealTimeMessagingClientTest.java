03975ccae02ae47ca4dac0ca48df4092d670bf65
allbegray <allbegray@gmail.com>
2015-11-09 16:45:13 +0900

    clean code

--- src/test/java/flowctrl/integration/slack/SlackRealTimeMessagingClientTest.java
+++ src/test/java/flowctrl/integration/slack/SlackRealTimeMessagingClientTest.java
@@ -20,10 +20,10 @@ public class SlackRealTimeMessagingClientTest {
 	@Test(timeout = 60 * 1000)
 	public void proxyTest() {
 		String protocol = "https";
-		String proxyUrl = "xxx.xxx.xxx.xxx";
+		String proxyHost = "xxx.xxx.xxx.xxx";
 		int proxyPort = 1234;
 
-		ProxyServerInfo proxyServerInfo = new ProxyServerInfo(protocol, proxyUrl, proxyPort);
+		ProxyServerInfo proxyServerInfo = new ProxyServerInfo(protocol, proxyHost, proxyPort);
 
 		SlackRealTimeMessagingClient realTimeMessagingClient = SlackClientFactory.createSlackRealTimeMessagingClient(token, proxyServerInfo);
 		realTimeMessagingClient.addListener("hello", new HelloEventListener());
