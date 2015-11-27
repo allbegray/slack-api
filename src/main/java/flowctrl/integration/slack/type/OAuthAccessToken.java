ab67142a5d1126ca03bf5fe6e30e4e802edbd2a7
allbegray <allbegray@gmail.com>
2015-11-27 11:48:09 +0900

    add field "OAuthAccessToken"

--- src/main/java/flowctrl/integration/slack/type/OAuthAccessToken.java
+++ src/main/java/flowctrl/integration/slack/type/OAuthAccessToken.java
@@ -1,5 +1,10 @@
 package flowctrl.integration.slack.type;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -7,6 +12,8 @@ public class OAuthAccessToken {
 
 	protected String access_token;
 	protected String scope;
+	protected String team_id;
+	protected String team_name;
 
 	public String getAccess_token() {
 		return access_token;
@@ -14,6 +21,13 @@ public class OAuthAccessToken {
 
 	public void setAccess_token(String access_token) {
 		this.access_token = access_token;
+	}
+
+	public Set<String> getScopes() {
+		if (scope == null) {
+			return Collections.emptySet();
+		}
+		return new HashSet<String>(Arrays.asList(scope.split(",")));
 	}
 
 	public String getScope() {
@@ -24,9 +38,25 @@ public class OAuthAccessToken {
 		this.scope = scope;
 	}
 
+	public String getTeam_id() {
+		return team_id;
+	}
+
+	public void setTeam_id(String team_id) {
+		this.team_id = team_id;
+	}
+
+	public String getTeam_name() {
+		return team_name;
+	}
+
+	public void setTeam_name(String team_name) {
+		this.team_name = team_name;
+	}
+
 	@Override
 	public String toString() {
-		return "OAuthAccessToken [access_token=" + access_token + ", scope=" + scope + "]";
+		return "OAuthAccessToken [access_token=" + access_token + ", scope=" + scope + ", team_id=" + team_id + ", team_name=" + team_name + "]";
 	}
 
 }
